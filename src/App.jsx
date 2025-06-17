4f315e5f2a523a811195d462affe43aaa92542c2
auilk <abdelghafour.lbaz01@gmail.com>
2025-06-17 10:58:18 +0100

    Set up project with Vite and integrate Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App()
+{
+  return(
+    <div className="w-dvw h-dvh flex justify-center items-center">
+      <h1 className="text-7xl font-bold">Yet Another Calculator</h1>
+    </div>
+  );
+}
+
+export default App;
